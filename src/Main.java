47169d4fd2e6737f856949c7717dd3f91024ceeb
MadalinCo <colibamadalin@gmail.com>
2023-03-09 12:54:26 +0200

    first experiment commitment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("This is an esperiment");
         System.out.println("Hello!");
     }
 }
